925f49852c49b43770957d08bab5fd2fb47ad05f
Jeff <jeff@JeffdeMBP.Home>
2018-05-04 23:26:26 +0800

    Add underscore library

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var path = require('path');
 import open from 'open';
+import _ from 'underscore';
 
 const port = 3000;
 const app = express();
 
 app.get('/', function (req, res) {
+
+    console.log(
+        _.shuffle([1, 2, 3, 4, 5, 6])
+    );
     res.sendFile(path.join(__dirname, '../src/index.html'));
 });  
 
