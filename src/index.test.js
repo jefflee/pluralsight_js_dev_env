a5ffb61c73c1c8efbde363a2be0730044b91f0eb
Jeff <jeff@JeffdeMBP.Home>
2018-05-08 00:01:03 +0800

    bug fix for JSDOM unit testing

--- src/index.test.js
+++ src/index.test.js
@@ -13,9 +13,8 @@ describe('index.html', () => {
         const index = fs.readFileSync('./src/index.html', "utf-8");
         jsdom.env(index, function (err, window) {
             const h1 = window.document.getElementsByTagName('h1')[0];
-            done();
             expect(h1.innerHTML).to.equal("Hello World!");
-            
+            done();
             window.close();
         });
     })
