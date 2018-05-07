3a7f12d64aef7d997da9e47242fe318ba7c8b366
Jeff <jeff@JeffdeMBP.Home>
2018-05-08 00:36:43 +0800

    Use fetch to make a ajax call and display data in the page.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import './index.css';
 import numeral from 'numeral';
+import { getUsers } from './api/userApi';
 
 /* eslint-disable no-console */
 
 const courseValue = numeral(1000).format('$0, 0.00');
 //debugger;
 console.log(`I would pay ${courseValue} for this awesome course!`);
+
+
+// Populate table of users via API call.
+getUsers().then(result => {
+    let usersBody = "";
+
+    result.forEach(user => {
+        usersBody += `<tr>
+      <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+      <td>${user.id}</td>
+      <td>${user.firstName}</td>
+      <td>${user.lastName}</td>
+      <td>${user.email}</td>
+      </tr>`
+    });
+
+    global.document.getElementById('users').innerHTML = usersBody;
+});
