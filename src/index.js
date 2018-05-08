205e4a6c053430359919daf5dea4e1c3703f8092
Jeff <jeff@JeffdeMBP.Home>
2018-05-09 00:00:08 +0800

    Add delete user in the page.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './index.css';
 import numeral from 'numeral';
-import { getUsers } from './api/userApi';
+import { getUsers, deleteUser } from './api/userApi';
 
 /* eslint-disable no-console */
 
@@ -24,4 +24,18 @@ getUsers().then(result => {
     });
 
     global.document.getElementById('users').innerHTML = usersBody;
+
+    const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+    // Must use array.from to create a real array from a DOM collection
+    // getElementsByClassname only returns an "array like" object
+    Array.from(deleteLinks, link => {
+        link.onclick = function (event) {
+            const element = event.target;
+            event.preventDefault();
+            deleteUser(element.attributes["data-id"].value);
+            const row = element.parentNode.parentNode;
+            row.parentNode.removeChild(row);
+        };
+    });
 });
