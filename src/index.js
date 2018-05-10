aac6bf8f345233265be17cf4c22394adc1084797
Jeff <jeff@JeffdeMBP.Home>
2018-05-11 00:24:17 +0800

    disable aslant rule - no-debugger

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import numeral from 'numeral';
 import { getUsers, deleteUser } from './api/userApi';
 
 /* eslint-disable no-console */
+/* eslint-disable no-debugger */
 
 const courseValue = numeral(1000).format('$0, 0.00');
 //debugger;
@@ -12,7 +13,7 @@ console.log(`I would pay ${courseValue} for this awesome course!`);
 // Populate table of users via API call.
 getUsers().then(result => {
     let usersBody = "";
-
+    
     result.forEach(user => {
         usersBody += `<tr>
       <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
