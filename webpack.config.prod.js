e2c49fb66295073e2bea2aa8ff615dda673cfb4f
Jeff <jeff@JeffdeMBP.Home>
2018-05-11 23:46:55 +0800

    separate CSS file from JS file.

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,6 +2,7 @@ import path from 'path';
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import WebpackMd5Hash from 'webpack-md5-hash';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 export default {
     debug: true,
@@ -18,6 +19,9 @@ export default {
         filename: '[name].[chunkhash].js'
     },
     plugins: [
+        // Generate an external css file with a hash in the filename
+    new ExtractTextPlugin('[name].[contenthash].css'),
+
         // Hash the files using MD5 so that their names change when the content changes.
         new WebpackMd5Hash(),
 
@@ -54,7 +58,7 @@ export default {
     module: {
         loaders: [
             { test: /\.js$/, exclude: /node_modules/, loaders: ['babel'] },
-            { test: /\.css$/, loaders: ['style', 'css'] }
+            {test: /\.css$/, loader: ExtractTextPlugin.extract('css?sourceMap')}
         ]
     }
 }
